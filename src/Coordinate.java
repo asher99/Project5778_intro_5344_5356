fb9146783557803d47d7da32541391da1457bfdb
אשר אלכסנדר <asherb94@gmail.com>
2018-03-08 14:20:32 +0200

    coordinate

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,8 +1,33 @@
 
+
+
 public class Coordinate {
 
-    private double coord;
-    public Coordinate(double x, double y){
+public double coord;
 
+// constant value for assumption of calculations
+final static double EPSILON = 0.00001;
+
+    public Coordinate(double newCoord){
+        this.coord = newCoord;
+    }
+
+
+    public double getCoord() {
+        return coord;
+    }
+
+    public void setCoord(double coord) {
+        this.coord = coord;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
