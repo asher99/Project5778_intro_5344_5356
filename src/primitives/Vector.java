caf7d13b69a1ba54973e55d6f97d280b3a31986a
zveia <zveialiezer04@gmail>
2018-03-12 12:22:46 +0200

    "Hello World"

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,31 +1,39 @@
 package primitives;
 
+/**
+ *  class vector for a vector in 3D space.
+ *  any vector has a size: the distance from the origin,
+ *  and a direction: represent by the three coordinates combination.
+ */
 public class Vector {
 
+    // any vector is based on a 3d point
     protected Point3D vector;
 
     // ***************** Constructor ********************** //
+    // build vector upon three coordinate values.
     public Vector(double x, double y, double z) {
         vector = new Point3D(x,y,z);
     }
 
+    // build vector using an existing 3D point.
     public Vector(Point3D vec) {
         vector = vec;
     }
 
     // ***************** Operations ******************** //
-    public Vector add(Vector a , Vector b){
-        return null;
-    }
 
+    // execute a "substract" operations on two vectors, and return the output vector.
     public Vector VectorialSubstract(Vector a , Vector b){
       return new Vector(Point3D.substract(a.vector,b.vector)) ;
     }
 
+    // execute a "add" operations on two vectors, and return the output vector.
     public Vector VectorialAdd(Vector a , Vector b){
         return new Vector(Point3D.add(a.vector,b.vector)) ;
     }
 
+    // multiply a vector by real scalar and return the output vector.
     public Vector multiplyByScalar(double scalar){
         return new Vector(
                 vector.getX() * scalar,
@@ -33,18 +41,21 @@ public class Vector {
                 vector.getZ() * scalar);
     }
 
+    // execute a "dot product" operations on two vectors, and return the output scalar.
     public double dotProduct(Vector a, Vector b){
         return  a.vector.getX() * b.vector.getX() +
                 a.vector.getY() * b.vector.getY() +
                 a.vector.getZ() * b.vector.getZ() ;
     }
 
+    // execute a "cross product" operations on two vectors, and return the output vector.
     public Vector crossProduct(Vector a , Vector b){
         return new Vector(a.vector.getY() * b.vector.getZ() - a.vector.getZ()* b.vector.getY(),
                           -(a.vector.getX() * b.vector.getZ() - a.vector.getZ()* b.vector.getX()),
                          a.vector.getX()* b.vector.getY() - a.vector.getY()*b.vector.getX());
     }
 
+    // return the size of the vector. this is the squere root of X^2 + Y^2 + Z^2.
     public double sizeOfVector(){
         return Point3D.distance(vector, new Vector(0,0,0).vector);
     }
