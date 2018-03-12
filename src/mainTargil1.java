75079279a856c8886eb63f1e59a48568c8d6ca8a
אשר אלכסנדר <asherb94@gmail.com>
2018-03-12 16:03:23 +0200

    main1

--- src/mainTargil1.java
+++ src/mainTargil1.java
@@ -14,10 +14,21 @@ public class mainTargil1 {
         System.out.println(vector2.toString());
 
         // cross product Example
-        Vector vector3 = crossProduct(vector1,vector2);
+        Vector vector3 = Vector.crossProduct(vector1,vector2);
         System.out.println(vector3);
 
+        // multiply by scalar Example
         Vector vector4 = new Vector(5,0.5,3);
         System.out.println(vector4.multiplyByScalar(1.6));
+
+        // size of vector Example
+        System.out.println(vector1.sizeOfVector());
+
+        // dot product Example
+        System.out.println(Vector.dotProduct(vector1,vector4));
+
+
+
+
     }
 }
