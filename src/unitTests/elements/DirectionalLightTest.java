8aca9291d906741b0bdb5d0fa704180f6518f5c0
אשר אלכסנדר <asherb94@gmail.com>
2018-05-17 15:57:38 +0300

    direction2...

--- src/unitTests/elements/DirectionalLightTest.java
+++ src/unitTests/elements/DirectionalLightTest.java
@@ -18,12 +18,12 @@ public class DirectionalLightTest {
                 new Point3D(-130, 130, -40),//bottom left
                 new Point3D(130, 130, -40),//bottom right
                 new Point3D(130, -130, -50),//top right
-                new Color(20, 20, 20), new Material(0.15, 0.05, 1));
+                new Color(20, 20, 20), new Material(0.015, 0.005, 0));
         Triangle second = new Triangle(
                 new Point3D(130, -130, -50),//top right
                 new Point3D(-130, -130, -50),//top left
                 new Point3D(-130, 130, -40),//bottom left
-                new Color(20, 20, 20), new Material(0.15, 0.05, 1));
+                new Color(20, 20, 20), new Material(0.015, 0.005, 0));
         Camera camera = new Camera(new Point3D(0, 0, 0),
                 new Vector(0, -1, 0),
                 new Vector(0, 0, -1));
@@ -36,7 +36,8 @@ public class DirectionalLightTest {
         myScene.setSceneAmbientLight(new AmbientLight(new Color(0, 0, 0), 0.1));
 
         DirectionalLight myDirectionalLight = new DirectionalLight(
-                new Color(255, 150, 150),new Vector(0, 1, -26));
+                new Color(255, 0, 0),
+                new Vector(0, 1, -39));
         myScene.addLightSource(myDirectionalLight);
 
 
@@ -51,4 +52,35 @@ public class DirectionalLightTest {
         myRender.getImageWriter().writeToimage();
     }
 
+    @Test
+    public void directionalight1() {
+
+        Sphere middle = new Sphere(new Point3D(0, 0, -50),
+                49,new Color(0,0,0),new Material(1, 1,1));
+
+        Camera camera = new Camera(new Point3D(0, 0, 0),
+                new Vector(0, -1, 0),
+                new Vector(0, 0, -1));
+        Scene myScene = new Scene("sphere in the sun");
+        myScene.setCameraScreenDistance(50);
+        myScene.setSceneCamera(camera);
+        myScene.setSceneBackgroundColor(new java.awt.Color(0, 0, 0));
+        myScene.addGeometries(middle);
+        myScene.setSceneAmbientLight(new AmbientLight(new Color(20, 20, 20), 0));
+
+        DirectionalLight myDirectionalLight = new DirectionalLight(
+                new Color(20, 0, 0),
+                new Vector(-3, 3, -3));
+        myScene.addLightSource(myDirectionalLight);
+
+        ImageWriter sceneWriter = new ImageWriter("sphere in the sun",1000,1000,1000,1000);
+        Render myRender = new Render();
+        myRender.setScene(myScene);
+        myRender.setImageWriter(sceneWriter);
+
+        myRender.renderImage();
+
+        //myRender.printGrid(100);
+        myRender.getImageWriter().writeToimage();
+    }
 }
