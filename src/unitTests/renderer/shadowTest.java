8c6cf6bbfef3a816bef1a2efb520c022284a55a6
אשר אלכסנדר <asherb94@gmail.com>
2018-06-04 14:19:51 +0300

    reflected2

--- src/unitTests/renderer/shadowTest.java
+++ src/unitTests/renderer/shadowTest.java
@@ -104,56 +104,5 @@ public class shadowTest {
 
     }
 
-    @Test
-    public void reflection() {
 
-        Triangle first = new Triangle(
-                new Point3D(-130, 130, -25),//bottom left
-                new Point3D(130, 130, -25),//bottom right
-                new Point3D(130, -130, -35),//top right
-                new Color(40, 40, 40), new Material(4, 0.2,1,0, 5));
-
-        Triangle second = new Triangle(
-                new Point3D(130, -130, -35),//top right
-                new Point3D(-130, -130, -35),//top left
-                new Point3D(-130, 130, -25),//bottom left
-                new Color(40, 40, 40), new Material(4, 0.2,1,0, 5));
-
-        Sphere middle = new Sphere(new Point3D(0, 0, -30),
-                20, new Color(0, 20, 100), new Material(1, 1,0,0, 20));
-
-        Camera camera = new Camera(new Point3D(0, 0, 0),
-                new Vector(0, -1, 0),
-                new Vector(0, 0, -1));
-
-        Scene myScene = new Scene("reflection test");
-        myScene.setCameraScreenDistance(70);
-        myScene.setSceneCamera(camera);
-        myScene.setSceneBackgroundColor(new java.awt.Color(0, 0, 0));
-        myScene.addGeometries(first, second,middle);
-        myScene.setSceneAmbientLight(new AmbientLight(new Color(0, 0, 0), 0.1));
-
-        SpotLight mySpotLight = new SpotLight(
-                new Point3D(12, 12, 0),
-                1, 0.00075, 0.0008,
-                new Color(255, 255, 255), new Vector(-12, -12, -15));
-
-        DirectionalLight myDirectionalLight = new DirectionalLight(new Color(40,0,0),new Vector(1.5,-3,-1));
-        myScene.addLightSource(mySpotLight);
-        myScene.addLightSource(myDirectionalLight);
-
-
-
-        ImageWriter sceneWriter = new ImageWriter("reflection test", 1000, 1000, 1000, 1000);
-        Render myRender = new Render();
-        myRender.setScene(myScene);
-        myRender.setImageWriter(sceneWriter);
-
-        myRender.renderImage();
-        myRender.renderPixel(450,450);
-        //myRender.printGrid(50);
-        myRender.getImageWriter().writeToimage();
-
-
-    }
 }
